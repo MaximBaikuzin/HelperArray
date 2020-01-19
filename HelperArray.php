db95da1c36cde4499d69b4ec5821d840c9a5ba85
Maxim Baikuzin <maxim@baikuzin.com>
2020-01-20 01:40:29 +0600

    Update HelperArray.php

--- HelperArray.php
+++ HelperArray.php
@@ -1,10 +1,16 @@
 <?php
-namespace MaximBaikuzin\Helper;
+namespace Helper;
 
 
 class HelperArray
 {
 
+    /**
+     * Pretty print is used to format the data.
+     * The data can be represented in a more readable form for humans by using pretty printing.
+     *
+     * @param array $arr
+     */
     public static function printPretty(array $arr): void
     {
         print('<pre>' . print_r($arr, true) . '</pre>');
